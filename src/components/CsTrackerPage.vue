58178dd2781124f23ecb096de72ecd2a057f2987
K4iyzen <driss.khettab@gmail.com>
2025-03-14 16:54:36 +0100

    adding the main structure of the headers of the CSTracker page

---
+++ src/components/CsTrackerPage.vue
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+<template>
+    <div class="main">
+        <div class="banner">
+            <img>
+            <div class="topLeft-button">
+
+            </div>
+            <div class="player-info">
+                <img>
+                <span> 13 585 Views</span>
+                <span>ketazu</span>
+            </div>
+            <div class="tabs">
+                <a>Overview</a>
+                <a>Matches</a>
+                <a>Maps</a>
+                <a>Weapons</a>
+            </div>    
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.main{
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
